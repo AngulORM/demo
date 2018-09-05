afca44addc2826f1d5579ae61181d4b3d22bed2c
lowrencium <6582186+lowrencium@users.noreply.github.com>
2018-09-05 14:26:00 +0200

    feat: Base entity manager

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {async, TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,12 +17,12 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-rest-demo'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-rest-demo');
+    expect(app.title).toEqual('angular-reducers-demo');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-rest-demo!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-reducers-demo!');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TestEntity} from '../entities/test.entity';
 import {SecondEntity} from '../entities/second.entity';
 import {AngularRestModule} from '../../projects/angulorm/src/lib/angular-rest.module';
@@ -8,17 +8,20 @@ import {AngularRestModule} from '../../projects/angulorm/src/lib/angular-rest.mo
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngulORM demo';
 
   entities = AngularRestModule.entities;
 
   constructor() {
-    const entity = new TestEntity();
     const entity2 = new SecondEntity();
 
     setInterval(() => {
       this.entities = AngularRestModule.entities;
     }, 100);
+  }
+
+  async ngOnInit() {
+    const entity = await TestEntity.read(1).toPromise();
   }
 }
