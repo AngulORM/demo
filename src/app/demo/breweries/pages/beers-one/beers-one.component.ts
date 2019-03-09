103a555008844d83b1733e3ec32b2ee89e2e33a5
Émile Taverne <6582186+lowrencium@users.noreply.github.com>
2019-03-09 17:34:17 +0100

    feat: Adding beer CRUD and linking to brewery

---
+++ src/app/demo/breweries/pages/beers-one/beers-one.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable, Subscription} from 'rxjs';
+import {ActivatedRoute, Router} from '@angular/router';
+import {BeerEntity} from '../../entities';
+
+@Component({
+  selector: 'ngf-demo-page-beers-one',
+  templateUrl: './beers-one.component.html',
+  styleUrls: ['./beers-one.component.scss']
+})
+export class BeersOneComponent implements OnInit, OnDestroy {
+  public beer: Observable<BeerEntity>;
+
+  private routeSub: Subscription;
+
+  constructor(private route: ActivatedRoute, private router: Router) {
+
+  }
+
+  ngOnInit(): void {
+    this.routeSub = this.route.params.subscribe(params => {
+      if (params['id']) {
+        this.beer = <Observable<BeerEntity>>BeerEntity.read(+params['id']);
+      } else {
+        this.router.navigateByUrl('/breweries/beers');
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
+  }
+}
--- src/app/demo/breweries/pages/index.ts
+++ src/app/demo/breweries/pages/index.ts
@@ -1,4 +1,5 @@
 export * from './beers-edit/beers-edit.component';
 export * from './beers-list/beers-list.component';
+export * from './beers-one/beers-one.component';
 export * from './breweries-list/breweries-list.component';
 export * from './breweries-one/breweries-one.component';
